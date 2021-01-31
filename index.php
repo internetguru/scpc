106f035ac2058358a703ed21ebd9c49280ecada8
Marta Pavelka <pavelka@corsair.math.miami.edu>
2021-01-31 02:30:53 -0500

    remove unused line

--- index.php
+++ index.php
@@ -52,7 +52,6 @@ echo <<<EOT
       window.dataLayer = window.dataLayer || [];
       function gtag(){dataLayer.push(arguments);}
       gtag('js', new Date());
-    
       gtag('config', 'G-S1ZRPY85CD');
     </script>
 EOT;
