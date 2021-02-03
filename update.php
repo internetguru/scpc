e865c367d171d21a6868175ff4fa093a33b18e8a
Pavel Petržela <pavel@petrzela.eu>
2021-02-03 06:55:34 +0100

    output convert

--- update.php
+++ update.php
@@ -5,4 +5,4 @@ echo $pull = shell_exec("/usr/local/bin/git pull 2>&1");
 if ($pull == "Already up to date.\n") {
   exit;
 }
-echo shell_exec("/usr/local/bin/jupyter nbconvert --to python scpc.ipynb");
+echo shell_exec("/usr/local/bin/jupyter nbconvert --to python scpc.ipynb 2>&1");
