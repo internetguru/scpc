c3bb0cd1e03522a4eb7983d98b0fe600be08a227
Jiri George Pavelka <pavelka.iix@gmail.com>
2021-02-02 19:01:03 -1000

    Update update.php

--- update.php
+++ update.php
@@ -1,10 +1,8 @@
 <?php
 
 echo "<pre><code>";
-
-$pull = shell_exec("/usr/local/bin/git pull 2>&1");
-if ($pull != "Already up to date.\n") {
-  echo shell_exec("/usr/local/bin/jupyter nbconvert --to python scpc.ipynb 2>&1 && echo done || echo failed");
-} else {
-  echo "$pull";
+echo $pull = shell_exec("/usr/local/bin/git pull");
+if ($pull == "Already up to date.\n") {
+  exit;
 }
+echo shell_exec("/usr/local/bin/jupyter nbconvert --to python scpc.ipynb");
